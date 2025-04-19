42f74f0a7d5929b0db8db3d9afa3fd4c7cb4efed
Resha1231213 <ishmaevrishat97@gmail.com>
2025-04-20 02:29:41 +0500

    Update states.py

--- states/states.py
+++ states/states.py
@@ -1,3 +1,5 @@
+# states.py
+
 from aiogram.fsm.state import StatesGroup, State
 
 class GenerateState(StatesGroup):
@@ -7,14 +9,8 @@ class GenerateState(StatesGroup):
     ready = State()
 
 class FinalGenerateState(StatesGroup):
-    enter_text = State()             # 🟡 ВОТ ЭТО ОБЯЗАТЕЛЬНО
+    waiting_for_avatar = State()
+    enter_text = State()
     select_lang = State()
     select_format = State()
-    select_subtitles = State()
-    confirm_generation = State()
-    waiting_for_avatar = State()
-    waiting_for_voice = State()
-    waiting_for_script = State()
-    waiting_for_format = State()
-    waiting_for_subtitles = State()
-    waiting_for_confirmation = State()
+    with_subtitles = State()
