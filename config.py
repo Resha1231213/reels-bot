2c4d6f28f9034b5f199cfebff0b07c27fd773657
Resha1231213 <ishmaevrishat97@gmail.com>
2025-04-23 01:44:55 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,11 @@
 # config.py
 
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
 
-# Загрузка переменных окружения из .env файла (локально)
 load_dotenv()
 
-# 🔐 Получаем токен бота и Telegram ID админа из переменных окружения
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
-
-# ✅ Для отладки — можешь убрать эти строки позже
-print("DEBUG BOT_TOKEN:", BOT_TOKEN)
-print("DEBUG ADMIN_ID:", ADMIN_ID)
+HEYGEN_API_KEY = os.getenv("HEYGEN_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
