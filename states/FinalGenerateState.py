c02dd538c8585233afe90eb4e39bc25d8194d370
Resha1231213 <ishmaevrishat97@gmail.com>
2025-04-23 03:21:18 +0500

    Update FinalGenerateState.py

--- states/FinalGenerateState.py
+++ states/FinalGenerateState.py
@@ -1,11 +1,13 @@
-# states/FinalGenerateState.py
-
-from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.state import StatesGroup, State
 
 class FinalGenerateState(StatesGroup):
     waiting_for_avatar = State()
     waiting_for_voice = State()
-    enter_text = State()
+    enter_script = State()  # ← ВОТ ЭТОГО НЕ ХВАТАЕТ
+    enter_link = State()
+    competitor_ready = State()
     select_language = State()
     select_format = State()
     with_subtitles = State()
+    upload_font = State()
+    confirm_generate = State()
