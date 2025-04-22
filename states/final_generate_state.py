2b7d148074317a8fc6b095f79c895e3a910a0a0a
Resha1231213 <ishmaevrishat97@gmail.com>
2025-04-23 01:45:40 +0500

    Create final_generate_state.py

---
+++ states/final_generate_state.py
@@ -0,0 +1,11 @@
+# states/final_generate_state.py
+
+from aiogram.fsm.state import State, StatesGroup
+
+class FinalGenerateState(StatesGroup):
+    waiting_for_avatar = State()
+    waiting_for_voice = State()
+    enter_text = State()
+    select_language = State()
+    select_format = State()
+    with_subtitles = State()
